ffa36abe7006841020a8869f9d67da32d06614a1
vigneshv1cky <97196163+vigneshv1cky@users.noreply.github.com>
2025-01-05 13:37:14 +0530

    Csv to Dataframe

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,14 +1,24 @@
+import os
 import pandas as pd
 from glob import glob
 
 # --------------------------------------------------------------
 # Read single CSV file
 # --------------------------------------------------------------
+single_file_arc = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
+)
+
+single_file_gyr = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
 
 
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
 # --------------------------------------------------------------
+
+files = glob("../../data/raw/MetaMotion/*.csv")
 
 
 # --------------------------------------------------------------
@@ -16,9 +26,48 @@ from glob import glob
 # --------------------------------------------------------------
 
 
+data_path = "../../data/raw/MetaMotion/"
+f = files[0]
+
+participant = f.split("-")[0].replace(data_path, "")
+label = f.split("-")[1]
+category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+df = pd.read_csv(f)
+df["participant"] = participant
+df["label"] = label
+df["category"] = category
+
+
 # --------------------------------------------------------------
 # Read all files
 # --------------------------------------------------------------
+
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+for f in files:
+    participant = f.split("-")[0].replace(data_path, "")
+    label = f.split("-")[1]
+    category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+    df = pd.read_csv(f)
+    df["participant"] = participant
+    df["label"] = label
+    df["category"] = category
+
+    if "Accelerometer" in f:
+        df["set"] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+
+    if "Gyroscope" in f:
+        df["set"] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
 
 
 # --------------------------------------------------------------
